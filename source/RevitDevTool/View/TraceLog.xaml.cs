874302fc896540bfbf9dbf67b5e8de98836c7a20
Truong Giang Vu <truonggiangvu610@gmail.com>
2025-05-01 14:38:42 +0700

    update: installer
    - refactor code base
    - support [RevitTemplates](https://github.com/Nice3point/RevitTemplates)
    - Revit 21-26

---
+++ source/RevitDevTool/View/TraceLog.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.UI;
+
+namespace RevitDevTool.View;
+
+public partial class TraceLog : IDockablePaneProvider
+{
+    public TraceLog()
+    {
+        InitializeComponent();
+    }
+
+    public void SetupDockablePane(DockablePaneProviderData data)
+    {
+        data.FrameworkElement = this;
+
+        data.InitialState = new DockablePaneState
+        {
+            DockPosition = DockPosition.Right,
+        };
+
+        data.FrameworkElement.MinWidth = 320;
+    }
+}
