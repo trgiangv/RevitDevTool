1705b82ae26964ec5b294bbf220e0b7130e8b6f4
Truong Giang Vu <truonggiangvu610@gmail.com>
2025-05-01 14:38:42 +0700

    update: installer
    - refactor code base
    - support [RevitTemplates](https://github.com/Nice3point/RevitTemplates)
    - Revit 21-26

---
+++ source/RevitDevTool/UI/Binding/DelegateCommand.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Input;
+
+namespace RevitDevTool.UI.Binding;
+
+public class DelegateCommand : ICommand
+{
+    public Action<object> ExecuteCommand { get; set; }
+
+    public Func<object, bool> CanExecuteCommand { get; set; }
+
+    public DelegateCommand() { }
+
+    public DelegateCommand(Action<object> execute, Func<object, bool> canExecute)
+    {
+        ExecuteCommand = execute;
+        CanExecuteCommand = canExecute;
+    }
+
+    public bool CanExecute(object parameter)
+    {
+        if (CanExecuteCommand != null)
+        {
+            return CanExecuteCommand(parameter);
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public event EventHandler CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    public void Execute(object parameter)
+    {
+        ExecuteCommand(parameter);
+    }
+}
