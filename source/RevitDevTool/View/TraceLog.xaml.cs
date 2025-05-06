2ef6ae452f4b379396b8f473bcc996cc95d39e23
Truong Giang Vu <truonggiangvu610@gmail.com>
2025-05-06 23:37:09 +0700

    update: theme change mechanism

--- source/RevitDevTool/View/TraceLog.xaml.cs
+++ source/RevitDevTool/View/TraceLog.xaml.cs
@@ -1,21 +1,52 @@
-﻿using System.Windows.Controls;
+﻿using System.ComponentModel;
+using System.Windows;
 using Autodesk.Revit.UI;
+using MaterialDesignThemes.Wpf;
+using UIFramework;
 
 namespace RevitDevTool.View;
 
-public partial class TraceLog : IDockablePaneProvider
+public partial class TraceLog : IDisposable
 {
     public TraceLog()
     {
         InitializeComponent();
+#if REVIT2024_OR_GREATER
+        ApplicationTheme.CurrentTheme.PropertyChanged += ApplyTheme;
+#endif
     }
 
-    public void SetupDockablePane(DockablePaneProviderData data)
+#if REVIT2024_OR_GREATER
+    private void ApplyTheme(object? sender, PropertyChangedEventArgs args)
     {
-        data.VisibleByDefault = true;
-        data.InitialState = new DockablePaneState
+        if (args.PropertyName != nameof(ApplicationTheme.CurrentTheme.RibbonPanelBackgroundBrush)) return;
+        if (UIThemeManager.CurrentTheme.ToString() == ApplicationTheme.CurrentTheme.RibbonTheme.Name) return;
+    
+        var resourceDictionary = GetThemeResourceDictionary();
+        var materialDesignTheme = resourceDictionary.GetTheme();
+
+        var newTheme = UIThemeManager.CurrentTheme switch
         {
-            DockPosition = DockPosition.Left,
+            UITheme.Light => BaseTheme.Light,
+            UITheme.Dark => BaseTheme.Dark,
+            _ => throw new ArgumentOutOfRangeException()
         };
+        materialDesignTheme.SetBaseTheme(newTheme);
+        resourceDictionary.SetTheme(materialDesignTheme);
+    }
+    
+    private ResourceDictionary GetThemeResourceDictionary()
+    {
+        return Resources.MergedDictionaries.First(x => x is IMaterialDesignThemeDictionary);
+    }
+#endif
+
+    public void Dispose()
+    {
+#if REVIT2024_OR_GREATER
+        ApplicationTheme.CurrentTheme.PropertyChanged -= ApplyTheme;
+#endif
+        Resources.MergedDictionaries.Clear();
+        GC.SuppressFinalize(this);
     }
 }
