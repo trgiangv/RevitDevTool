e7168fd7eddbfaeb1f35eb8be03465215850daef
Truong Giang Vu <truonggiangvu610@gmail.com>
2025-05-09 07:35:34 +0700

    update: PolylineVisualizationServer render

--- source/RevitDevTool/Visualization/Render/RenderConfiguration.cs
+++ source/RevitDevTool/Visualization/Render/RenderConfiguration.cs
@@ -7,9 +7,9 @@ public static class BoundingBoxVisualizationSettings
 {
     public const double Transparency  = 60;
 
-    public static Color SurfaceColor  = Colors.DodgerBlue;
-    public static Color EdgeColor  = Color.FromArgb(255, 30, 81, 255);
-    public static Color AxisColor  = Color.FromArgb(255, 255, 89, 30);
+    public static readonly Color SurfaceColor  = Colors.DodgerBlue;
+    public static readonly Color EdgeColor  = Color.FromArgb(255, 30, 81, 255);
+    public static readonly Color AxisColor  = Color.FromArgb(255, 255, 89, 30);
 
     public const bool ShowSurface  = true;
     public const bool ShowEdge  = true;
@@ -18,28 +18,28 @@ public static class BoundingBoxVisualizationSettings
 
 public static class FaceVisualizationSettings
 {
-    public static double Transparency  = 20;
-    public static double Extrusion  = Context.Application.VertexTolerance * 12;
-    public static double MinExtrusion  = Context.Application.VertexTolerance * 12;
+    public const double Transparency  = 20;
+    public static readonly double Extrusion  = Context.Application.VertexTolerance * 12;
+    public static readonly double MinExtrusion  = Context.Application.VertexTolerance * 12;
 
-    public static Color SurfaceColor  = Colors.DodgerBlue;
-    public static Color MeshColor  = Color.FromArgb(255, 30, 81, 255);
-    public static Color NormalVectorColor  = Color.FromArgb(255, 255, 89, 30);
+    public static readonly Color SurfaceColor  = Colors.DodgerBlue;
+    public static readonly Color MeshColor  = Color.FromArgb(255, 30, 81, 255);
+    public static readonly Color NormalVectorColor  = Color.FromArgb(255, 255, 89, 30);
 
     public const bool ShowSurface  = true;
     public const bool ShowMeshGrid  = true;
     public const bool ShowNormalVector  = true;
 }
 
-public class MeshVisualizationSettings
+public static class MeshVisualizationSettings
 {
-    public static double Transparency  = 20;
-    public static double Extrusion  = Context.Application.VertexTolerance * 12;
-    public static double MinExtrusion  = Context.Application.VertexTolerance * 12;
+    public const double Transparency  = 20;
+    public static readonly double Extrusion  = Context.Application.VertexTolerance * 12;
+    public static readonly double MinExtrusion  = Context.Application.VertexTolerance * 12;
 
-    public static Color SurfaceColor  = Colors.DodgerBlue;
-    public static Color MeshColor  = Color.FromArgb(255, 30, 81, 255);
-    public static Color NormalVectorColor  = Color.FromArgb(255, 255, 89, 30);
+    public static readonly Color SurfaceColor  = Colors.DodgerBlue;
+    public static readonly Color MeshColor  = Color.FromArgb(255, 30, 81, 255);
+    public static readonly Color NormalVectorColor  = Color.FromArgb(255, 255, 89, 30);
 
     public const bool ShowSurface  = true;
     public const bool ShowMeshGrid  = true;
@@ -52,9 +52,9 @@ public static class PolylineVisualizationSettings
     public const double Diameter  = 2;
     public const double MinThickness  = 0.1;
 
-    public static Color SurfaceColor  = Colors.DodgerBlue;
-    public static Color CurveColor  = Color.FromArgb(255, 30, 81, 255);
-    public static Color DirectionColor  = Color.FromArgb(255, 255, 89, 30);
+    public static readonly Color SurfaceColor  = Colors.DodgerBlue;
+    public static readonly Color CurveColor  = Color.FromArgb(255, 30, 81, 255);
+    public static readonly Color DirectionColor  = Color.FromArgb(255, 255, 89, 30);
 
     public const bool ShowSurface  = true;
     public const bool ShowCurve  = true;
@@ -66,8 +66,8 @@ public static class SolidVisualizationSettings
     public const double Transparency  = 20;
     public const double Scale  = 1;
 
-    public static Color FaceColor  = Colors.DodgerBlue;
-    public static Color EdgeColor  = Color.FromArgb(255, 30, 81, 255);
+    public static readonly Color FaceColor  = Colors.DodgerBlue;
+    public static readonly Color EdgeColor  = Color.FromArgb(255, 30, 81, 255);
 
     public const bool ShowFace  = true;
     public const bool ShowEdge  = true;
@@ -79,9 +79,9 @@ public static class XyzVisualizationSettings
     public const double AxisLength  = 6;
     public const double MinAxisLength  = 0.1;
 
-    public static Color XColor  = Color.FromArgb(255, 30, 227, 255);
-    public static Color YColor  = Color.FromArgb(255, 30, 144, 255);
-    public static Color ZColor  = Color.FromArgb(255, 30, 81, 255);
+    public static readonly Color XColor  = Color.FromArgb(255, 30, 227, 255);
+    public static readonly Color YColor  = Color.FromArgb(255, 30, 144, 255);
+    public static readonly Color ZColor  = Color.FromArgb(255, 30, 81, 255);
 
     public const bool ShowPlane  = true;
     public const bool ShowXAxis  = true;
