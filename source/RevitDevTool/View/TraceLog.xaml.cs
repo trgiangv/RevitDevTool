5e3140259b758c200ff7d6621570414d8dfff847
Truong Giang Vu <truonggiangvu610@gmail.com>
2025-05-09 13:28:07 +0700

    update: enhance theme management and resource handling

--- source/RevitDevTool/View/TraceLog.xaml.cs
+++ source/RevitDevTool/View/TraceLog.xaml.cs
@@ -1,45 +1,33 @@
-﻿#if REVIT2024_OR_GREATER
+﻿using RevitDevTool.Theme;
+using ApplicationTheme = UIFramework.ApplicationTheme;
+#if REVIT2024_OR_GREATER
 using System.ComponentModel;
-using System.Windows;
 using Autodesk.Revit.UI;
-using MaterialDesignThemes.Wpf;
-using UIFramework;
 #endif
 
 namespace RevitDevTool.View;
 
 public partial class TraceLog : IDisposable
 {
+    private static readonly ThemeWatcher ThemeWatcher = new();
+    
     public TraceLog()
     {
         InitializeComponent();
 #if REVIT2024_OR_GREATER
         ApplicationTheme.CurrentTheme.PropertyChanged += ApplyTheme;
 #endif
+        ThemeWatcher.Initialize();
+        ThemeWatcher.Watch(this);
+        ThemeWatcher.ApplyTheme();
     }
 
 #if REVIT2024_OR_GREATER
-    private void ApplyTheme(object? sender, PropertyChangedEventArgs args)
+    private static void ApplyTheme(object? sender, PropertyChangedEventArgs args)
     {
         if (args.PropertyName != nameof(ApplicationTheme.CurrentTheme.RibbonPanelBackgroundBrush)) return;
         if (UIThemeManager.CurrentTheme.ToString() == ApplicationTheme.CurrentTheme.RibbonTheme.Name) return;
-    
-        var resourceDictionary = GetThemeResourceDictionary();
-        var materialDesignTheme = resourceDictionary.GetTheme();
-
-        var newTheme = UIThemeManager.CurrentTheme switch
-        {
-            UITheme.Light => BaseTheme.Light,
-            UITheme.Dark => BaseTheme.Dark,
-            _ => throw new ArgumentOutOfRangeException()
-        };
-        materialDesignTheme.SetBaseTheme(newTheme);
-        resourceDictionary.SetTheme(materialDesignTheme);
-    }
-    
-    private ResourceDictionary GetThemeResourceDictionary()
-    {
-        return Resources.MergedDictionaries.First(x => x is IMaterialDesignThemeDictionary);
+        ThemeWatcher.ApplyTheme();
     }
 #endif
 
