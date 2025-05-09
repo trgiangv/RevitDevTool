92cdc3e69c03f019cab8de5499a013c552c6c5b0
Truong Giang Vu <truonggiangvu610@gmail.com>
2025-05-09 13:28:07 +0700

    update: enhance theme management and resource handling

---
+++ source/RevitDevTool/Theme/ThemeWatcher.cs
@@ -0,0 +1,134 @@
+﻿using System.Windows;
+#if REVIT2024_OR_GREATER
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+#endif
+using Wpf.Ui;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
+using Color = System.Windows.Media.Color;
+namespace RevitDevTool.Theme;
+
+public sealed class ThemeWatcher
+{
+#if REVIT2024_OR_GREATER
+    private bool _isWatching;
+#endif
+
+    private readonly List<FrameworkElement> _observedElements = [];
+
+    public void Initialize()
+    {
+        UiApplication.Current.Resources = new ResourceDictionary
+        {
+            Source = new Uri("pack://application:,,,/RevitDevTool;component/Theme/Theme.xaml", UriKind.Absolute)
+        };
+
+        ApplicationThemeManager.Changed += OnApplicationThemeManagerChanged;
+    }
+
+    public void ApplyTheme()
+    {
+        var theme = ApplicationTheme.Dark;
+#if REVIT2024_OR_GREATER
+        theme = GetRevitTheme();
+
+        if (!_isWatching)
+        {
+            ExternalEventController.ActionEventHandler.Raise(application => application.ThemeChanged += OnRevitThemeChanged);
+            _isWatching = true;
+        }
+#endif
+        ApplicationThemeManager.Apply(theme);
+        UpdateBackground(theme);
+    }
+
+    public void Watch(FrameworkElement frameworkElement)
+    {
+        ApplicationThemeManager.Apply(frameworkElement);
+        frameworkElement.Loaded += OnWatchedElementLoaded;
+        frameworkElement.Unloaded += OnWatchedElementUnloaded;
+    }
+
+    public void Unwatch()
+    {
+#if REVIT2024_OR_GREATER
+        if (!_isWatching) return;
+
+        ExternalEventController.ActionEventHandler.Raise(application => application.ThemeChanged -= OnRevitThemeChanged);
+        _isWatching = false;
+#endif
+    }
+
+#if REVIT2024_OR_GREATER
+    private static ApplicationTheme GetRevitTheme()
+    {
+        return UIThemeManager.CurrentTheme switch
+        {
+            UITheme.Light => ApplicationTheme.Light,
+            UITheme.Dark => ApplicationTheme.Dark,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    private void OnRevitThemeChanged(object? sender, ThemeChangedEventArgs args)
+    {
+        if (args.ThemeChangedType != ThemeType.UITheme) return;
+
+        if (_observedElements.Count > 0)
+        {
+            _observedElements[0].Dispatcher.Invoke(ApplyTheme);
+        }
+    }
+#endif
+
+    private void OnApplicationThemeManagerChanged(ApplicationTheme applicationTheme, Color accent)
+    {
+        foreach (var frameworkElement in _observedElements)
+        {
+            ApplicationThemeManager.Apply(frameworkElement);
+            UpdateDictionary(frameworkElement);
+        }
+    }
+
+    private void OnWatchedElementLoaded(object sender, RoutedEventArgs e)
+    {
+        var element = (FrameworkElement) sender;
+        _observedElements.Add(element);
+
+        if (element.Resources.MergedDictionaries[0].Source.OriginalString != UiApplication.Current.Resources.MergedDictionaries[0].Source.OriginalString)
+        {
+            ApplicationThemeManager.Apply(element);
+            UpdateDictionary(element);
+        }
+    }
+
+    private void OnWatchedElementUnloaded(object sender, RoutedEventArgs e)
+    {
+        var element = (FrameworkElement) sender;
+        _observedElements.Remove(element);
+    }
+
+    private static void UpdateDictionary(FrameworkElement frameworkElement)
+    {
+        var themedResources = frameworkElement.Resources.MergedDictionaries
+            .Where(dictionary => dictionary.Source.OriginalString.Contains("RevitDevTool.UI;", StringComparison.OrdinalIgnoreCase))
+            .ToArray();
+
+        frameworkElement.Resources.MergedDictionaries.Insert(0, UiApplication.Current.Resources.MergedDictionaries[0]);
+        frameworkElement.Resources.MergedDictionaries.Insert(1, UiApplication.Current.Resources.MergedDictionaries[1]);
+
+        foreach (var themedResource in themedResources)
+        {
+            frameworkElement.Resources.MergedDictionaries.Remove(themedResource);
+        }
+    }
+
+    private void UpdateBackground(ApplicationTheme theme)
+    {
+        foreach (var window in _observedElements.Select(Window.GetWindow).Distinct())
+        {
+            WindowBackgroundManager.UpdateBackground(window, theme, WindowBackdropType.Mica);
+        }
+    }
+}
