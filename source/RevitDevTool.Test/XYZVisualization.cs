1f2d803fe16c834faa59838a3c8d1a3df6309939
Truong Giang Vu <truonggiangvu610@gmail.com>
2025-06-02 00:56:55 +0700

    add: test project

---
+++ source/RevitDevTool.Test/XYZVisualization.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI.Selection;
+using Nice3point.Revit.Toolkit.External;
+
+namespace RevitDevTool.Test;
+
+[Transaction(TransactionMode.Manual)]
+[UsedImplicitly]
+public class XyzVisualization : ExternalCommand
+{
+    public override void Execute()
+    {
+        try
+        {
+            var xyz = UiDocument.Selection.PickPoint();
+            Trace.Write(xyz);
+        }
+        catch (Exception e)
+        {
+            Trace.TraceError($"Error in XyzVisualization: {e.Message}");
+        }
+    }
+}
+
+[Transaction(TransactionMode.Manual)]
+[UsedImplicitly]
+public class XyzsVisualization : ExternalCommand
+{
+    public override void Execute()
+    {
+        try
+        {
+            var xyzRefs = UiDocument.Selection.PickObjects(ObjectType.PointOnElement);
+            var xyzs = xyzRefs.Select(x=>x.GlobalPoint).ToList();
+            if (xyzs.Count == 0)
+            {
+                Trace.TraceWarning("No points selected.");
+                return;
+            }
+            Trace.Write(xyzs);
+        }
+        catch (Exception e)
+        {
+            Trace.TraceError($"Error in XyzsVisualization: {e.Message}");
+        }
+    }
+}
