63fd7186ca907f3979d1347780c1f779651739ef
Truong Giang Vu <truonggiangvu610@gmail.com>
2025-06-02 00:56:55 +0700

    add: test project

---
+++ source/RevitDevTool.Test/MeshVisualization.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI.Selection;
+using Nice3point.Revit.Toolkit.External;
+using RevitDevTool.Test.Extensions;
+
+namespace RevitDevTool.Test;
+
+[Transaction(TransactionMode.Manual)]
+[UsedImplicitly]
+public class MeshVisualization : ExternalCommand
+{
+    public override void Execute()
+    {
+        try
+        {
+            var meshRef = UiDocument.Selection.PickObject(ObjectType.Element, "Select Mesh Element");
+            var mesh = Document.GetElement(meshRef).GetMeshes();
+            Trace.Write(mesh.First());
+        }
+        catch (Exception e)
+        {
+            Trace.TraceError($"Error in MeshVisualization: {e.Message}");
+        }
+    }
+}
+
+[Transaction(TransactionMode.Manual)]
+[UsedImplicitly]
+public class MeshesVisualization : ExternalCommand
+{
+    public override void Execute()
+    {
+        try
+        {
+            var meshRefs = UiDocument.Selection.PickObjects(ObjectType.Element, "Select Mesh Elements");
+            var meshes = meshRefs.SelectMany(mRef => Document.GetElement(mRef).GetMeshes()).ToList();
+            Trace.Write(meshes);
+        }
+        catch (Exception e)
+        {
+            Trace.TraceError($"Error in MeshesVisualization: {e.Message}");
+        }
+    }
+}
